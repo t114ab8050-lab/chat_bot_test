19745951106313ea59abff68fedd2099ce6e9941
t114ab8050-lab <t114ab8050@ntut.org.tw>
2026-02-01 23:17:49 +0800

    upload

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+# 從環境變數中讀取設定
+# 請在 Heroku 的 Settings -> Config Vars 中設定這些變數
+line_bot_api = LineBotApi(os.environ.get('SV5hlL1NXD9QOzbKSXF5Hzk6+AOo+dAwtEaSOHz9IBDcpSEnoEoXJ2VCUjBQHa8sX6EWknwS18mRoBGyomAIaWJQnirilGEV1t/C8zth8rTP71UajtdrXtTDh40Yk+i0N7Hw2wcZ5rS24HiJcFkBkAdB04t89/1O/w1cDnyilFU='))
+handler = WebhookHandler(os.environ.get('2e9e73781d0e66c8e92fed2aa050c3bb'))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # 取得 X-Line-Signature 表頭 (確保訊息來自 LINE)
+    signature = request.headers['X-Line-Signature']
+
+    # 以文字形式取得請求內容
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # 處理 Webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+# 處理文字訊息的事件
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # 回覆相同的訊息 (Echo)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
